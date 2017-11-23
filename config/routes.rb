81148ac2854a2d2555fe2fad0756c26ce4855e32
gregory blain <gregory.blain@gmail.com>
2017-11-23 11:56:44 +0100

    ajout seed et controller ingredients doses

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'doses/new'
+
+  get 'doses/destroy'
+
+  get 'cocktails/index'
+
+  get 'cocktails/show'
+
+  get 'cocktails/new'
+
+  get 'cocktails/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
